504323106d3bc757b21a08eda506ffa233bf55a4
Sane4eks <alexandrtaranukhin@gmail.com>
2024-09-20 23:34:05 +0200

    Обновлён код для работы с базой данных

--- database.py
+++ database.py
@@ -8,6 +8,19 @@ def connect():
         print(f"Error connecting to database: {e}")
         return None
 
+# Функция для создания таблицы, если она не существует
+def create_table():
+    with connect() as conn:
+        if conn:
+            cursor = conn.cursor()
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS users (
+                    user_id INTEGER PRIMARY KEY,
+                    username TEXT
+                )
+            ''')
+            conn.commit()
+
 # Функция для добавления пользователя в базу данных
 def insert_user(user_id: int, username: str):
     with connect() as conn:
@@ -28,3 +41,6 @@ def get_all_users():
             cursor = conn.cursor()
             cursor.execute('SELECT * FROM users')
             return cursor.fetchall()
+
+# Вызов функции для создания таблицы
+create_table()
